8e8cf725e0a64131b80b856d4336ef279921fc09
fati <faticarolinamelgarejo2@gmail.com>
2023-07-16 15:41:08 -0300

    segunda correccion

--- src/server.js
+++ src/server.js
@@ -37,48 +37,6 @@ app.use('/api/carts', CartRouter); // Ruta para las operaciones de carritos
 app.use('/', viewsRouter); // Ruta para las vistas
 
 
-
-// Ruta para manejar la carga de imágenes desde el formulario
-app.post('/productos', upload.single('thumbnails'), (req, res) => {
-  try {
-    // Aquí puedes acceder a los datos del formulario (req.body) y la información del archivo cargado (req.file)
-    const formData = req.body;
-    const imageFile = req.file;
-
-    // Procesa la información y guarda la imagen en la ubicación que desees
-    // Por ejemplo, podrías mover el archivo a una carpeta de "uploads" y guardar el nombre del archivo en tu base de datos
-    if (imageFile) {
-      // Obtén el nombre original del archivo cargado
-      const originalFileName = imageFile.originalname;
-
-      // Define la ubicación donde se guardará el archivo de imagen
-      const uploadPath = path.join(__dirname, 'Public/uploads/', originalFileName);
-
-      // Mueve el archivo a la ubicación deseada
-      fs.renameSync(imageFile.path, uploadPath);
-
-      // Guarda el nombre del archivo de imagen en la base de datos o realiza otras acciones necesarias
-      // ...
-
-      // Envía una respuesta adecuada al cliente
-      res.status(200).json({ message: 'Imagen cargada correctamente.' });
-    } else {
-      // Si no se cargó ninguna imagen
-      res.status(400).json({ error: 'Debe cargar una imagen.' });
-    }
-  } catch (error) {
-    // Si ocurre algún error durante el proceso de carga y almacenamiento de la imagen
-    res.status(500).json({ error: 'Error al cargar la imagen.' });
-  }
-  const imageName = req.file.filename;
-  console.log('Nombre del archivo cargado:', imageName);
-});
-
-  
-
-  // Resto del código para guardar el nombre de la imagen en la base de datos u otras acciones necesarias
-
-
 const port = 8080; // Puerto en el que se ejecutará el servidor HTTP
 server.listen(port, () => {
   console.log(`Servidor HTTP escuchando en el puerto ${port}`);
